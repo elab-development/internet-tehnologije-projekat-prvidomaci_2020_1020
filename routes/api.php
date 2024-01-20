d336a21472fe4fe461bbb1737fbbc047dfe95850
vanjaparezanovic <vanja.parezanovic@gmail.com>
2024-01-20 20:34:27 +0100

    Added controllers + routes to api

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,26 @@ use App\Http\Controllers\API\AuthController;
 |
 */
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+
+Route::resource('/books', BookController::class);
+Route::resource('users.books', UserBookController::class)->only(['index']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+    Route::resource('/books', BookController::class)->only(['update','store','destroy']);
+
+    // API route for logout user
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
